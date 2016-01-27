b05124e8895621592cd06e70196d3902f3d83888
Mike iLL Kilmer <mike@mzoo.org>
2016-01-27 09:40:59 -0600

    Remove unneeded directories.

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 
-rsync -avP screenshot.png templates *.php *.css vr:public_html/storybooksound/wp/wp-content/themes/14stories
+rsync -avP *.php *.css vr:public_html/storybooksound/wp/wp-content/themes/14stories
 
